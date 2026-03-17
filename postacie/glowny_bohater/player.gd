f1b75493cd1a8aae37baad802187d75cf29f254e
Zodiax12 <olewu@wp.pl>
2026-03-17 20:07:33 +0100

    Transform
    transformacja szczura i integracja z poprzednim commitem

--- postacie/glowny_bohater/player.gd
+++ postacie/glowny_bohater/player.gd
@@ -1,8 +1,11 @@
 extends Area2D
 @export var speed = 200 
 var screen_size
+var magik = load("res://grafiki/szczur_transform.png")
+
 func _ready():
 	screen_size = get_viewport_rect().size
+	$Sprite2D.hide()
 
 func _process(delta):
 	var velocity = Vector2.ZERO	
@@ -15,6 +18,15 @@ func _process(delta):
 		velocity.y +=1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -=1
+		
+	if Input.is_action_pressed("transform"):
+		$Sprite2D.texture = magik
+		$Sprite2D.show()
+		$AnimatedSprite2D.hide()
+		$mieczyk_myszka.hide() 
+	if velocity.x != 0:
+		$Sprite2D.flip_v = false
+		$Sprite2D.flip_h = velocity.x > 0
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
